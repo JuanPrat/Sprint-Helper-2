b21292f8acbfebf25a3c1077a78c726ee7d53576
Juan Jose Garcia Sosa <juan.garcia@sofka.com.co>
2021-04-08 21:46:42 -0500

    creación de archivos base para el proyecto

---
+++ src/components/Formulario.js
@@ -0,0 +1,15 @@
+function Formulario() {
+  return (
+    <div>
+      <form>
+        <label>Codigo Hu</label>
+        <input type="text"></input>
+        <label>Puntos Hu</label>
+        <input type="text"></input>
+      </form>
+      <button>Enviar</button>
+    </div>
+  );
+}
+
+export default Formulario;
---
+++ src/components/ListaHistorias.js
@@ -0,0 +1,10 @@
+function ListaHistorias(props) {
+  return (
+    <div>
+      <ul>lista 1</ul>
+      <ul>lista 2</ul>
+    </div>
+  );
+}
+
+export default ListaHistorias;
