7152ca77d3077eb23505d21877c701a0a6da25a3
Juan Jose Garcia Sosa <juanprat96@gmail.com>
2021-04-14 00:25:04 -0500

    test dummy

--- src/components/ListaHistorias.test.js
+++ src/components/ListaHistorias.test.js
@@ -3,4 +3,6 @@ import { render, fireEvent} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ListaHistorias from './ListaHistorias'
 
-test("Aumenta ta")
+test("Aumenta ta", () => {
+    expect(true).toBe(true)
+})
