d70dc077696304032bd57a33cde2e150a3ef9c33
Juan Jose Garcia Sosa <juanprat96@gmail.com>
2021-04-14 23:03:35 -0500

    intentos fallidos de mockear localStorage para las prueabas de ListarHistorias

--- src/components/ListaHistorias.test.js
+++ src/components/ListaHistorias.test.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { render, fireEvent} from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ListaHistorias from './ListaHistorias'
 
-test("Aumenta ta", () => {
-    expect(true).toBe(true)
-})
+test('should save to localStorage', () => {
+    const KEY = 'foo',
+      VALUE = 'bar';
+    dispatch(action.update(KEY, VALUE));
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, VALUE);
+    expect(localStorage.__STORE__[KEY]).toBe(VALUE);
+    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+  });
