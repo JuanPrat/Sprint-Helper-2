00b3f1aa40af2990ff633c86b313e0e72feaa2d3
Juan Jose Garcia Sosa <juanprat96@gmail.com>
2021-04-20 20:59:59 -0500

    prueba unitaria para testear el agregado de nuevos registros

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent} from '@testing-library/react'
+import { render, fireEvent, queryByTestId, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Formulario from './Formulario'
 
@@ -21,5 +21,23 @@ test('inputs cambian de valor a string vacio cuando clickeamos', () => {
 
     expect(codigoInput).toHaveValue("");
     expect(puntosInput).toHaveValue("");
+})
+
+test('imprime los valores ingresados en el formulario', ()=> {
+    const { queryByTestId } = render(<Formulario />)
+
+    const codigoInput = queryByTestId('input-codigo')
+    const puntosInput = queryByTestId('input-puntos')
+    const enviarButton = queryByTestId('enviar-button')
+
+    userEvent.type(codigoInput, "12345")
+    userEvent.type(puntosInput, "8")
+    fireEvent.click(enviarButton)
+
+    const pCodigo = screen.queryByText("Codigo: 12345")
+    const pPuntos = screen.queryByText("Puntos: 8")
+    
+    expect(pCodigo).toBeTruthy()
+    expect(pPuntos).toBeTruthy()
 })
 
