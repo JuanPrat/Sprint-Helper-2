f174dfc7c46382f5c3266f27af66995cdb7c041d
Juan Jose Garcia Sosa <juanprat96@gmail.com>
2021-05-03 20:23:16 -0500

    desarrollo para calcular cuandos puntos se han ejecutado de una hisotria segun la fecha con la que fue creado

--- src/components/ListaHistorias.js
+++ src/components/ListaHistorias.js
@@ -3,9 +3,9 @@ import { readFromLocalStorage, saveInLocalStorage } from '../utils/LocalStorage'
 
 class ListaHistorias extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {  listaHu:  readFromLocalStorage()}
+    this.state = { listaHu: readFromLocalStorage() }
   }
 
   shouldComponentUpdate(nextProps) {// evalua si el componente deberia renderizarse. devuelve true o false
@@ -14,25 +14,48 @@ class ListaHistorias extends Component {
       huArray = []
     }
     const update = huArray.filter(hu => hu.codigo === nextProps.codigo).length === 0;
-    if(!update){
+    if (!update) {
       window.alert("Una historia de usuario con este codigo ya existe")
     }
     return update
   }
 
-  render() {
-    saveInLocalStorage(this.props.codigo, this.props.puntos,this.props.fecha, this.state.listaHu)
-    let huArray = []
+  calcularPuntosConsumidos(fechaHu) {
+    if (fechaHu !== undefined) {
+      const fecha1 = new Intl.DateTimeFormat('en-US').format(new Date(fechaHu))
+      const fecha2 = new Intl.DateTimeFormat('en-US').format(new Date())
+      const fechaActual = new Date()
+      const puntos = 2 * this.restaFechas(fecha1, fecha2);
+      if(fechaActual.getHours() < 12){
+        puntos = puntos -1 ;
+      }
+      return puntos
+    }
+  }
+
+  // Función para calcular los días transcurridos entre dos fechas
+  restaFechas = function (f1, f2) {
+    var aFecha1 = f1.split('/');
+    var aFecha2 = f2.split('/');
     debugger
-    console.log(new Intl.DateTimeFormat('en-US').format(this.props.fecha))
+    var fFecha1 = Date.UTC(aFecha1[2], aFecha1[0] , aFecha1[1]- 1);
+    var fFecha2 = Date.UTC(aFecha2[2], aFecha2[0] , aFecha2[1]- 1);
+    var dif = fFecha2 - fFecha1;
+    var dias = Math.floor(dif / (1000 * 60 * 60 * 24));
+    return dias;
+  }
+
+  render() {
+    saveInLocalStorage(this.props.codigo, this.props.puntos, this.props.fecha, this.state.listaHu)
+    let huArray = []
     this.state.listaHu.forEach(hu => {
       huArray.push(
-          <ul key={hu.codigo}>
-            <p>Codigo: {hu.codigo}</p>
-            <p>Puntos: {hu.puntos}</p>
-            <p>Creada: {new Intl.DateTimeFormat('en-US').format(new Date(hu.fecha))}</p>
-            <p>consumidos: </p>
-          </ul>
+        <ul key={hu.codigo}>
+          <p>Codigo: {hu.codigo}</p>
+          <p>Puntos: {hu.puntos}</p>
+          <p>Creada: {new Intl.DateTimeFormat('en-US').format(new Date(hu.fecha))}</p>
+          <p>consumidos: {this.calcularPuntosConsumidos(hu.fecha)}</p>
+        </ul>
       )
     }
     )
